2df344d01bdf278fcee81ccd5d75948a3c59dd05
tcracked <alkoboy90@gmail.com>
2020-02-17 18:48:01 +0300

    Step 3

--- Main.java
+++ Main.java
@@ -2,16 +2,78 @@ package tictactoe;
 
 import java.util.Scanner;
 
+
+
 public class Main {
+
+    public static boolean checkLine(char[][] arrCh, char ch) {
+        for (int i = 0; i < arrCh.length; i++) {
+            if ((arrCh[i][0] == ch && arrCh[i][1] == ch && arrCh[i][2] == ch) ||
+                    (arrCh[0][i] == ch && arrCh[1][i] == ch && arrCh[2][i] == ch)) {
+                return true;
+            }
+        }
+        return (arrCh[0][0] == ch && arrCh[1][1] == ch && arrCh[2][2] == ch) ||
+                (arrCh[2][0] == ch && arrCh[1][1] == ch && arrCh[0][2] == ch);
+    }
+
+    public static boolean isFull(char[][] arrCh) {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (arrCh[i][j] == '_') {
+                    return false;
+                }
+        return true;
+    }
+
+    public static int countChar(char[][] arrCh, char ch) {
+        int count = 0;
+        for (int i = 0; i < arrCh.length; i++) {
+            for (int j = 0; j < arrCh[i].length; j++) {
+                if (arrCh[i][j] == ch) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        char[] field;
+        char[][] field = new char[3][3];
         System.out.print("Enter cells: ");
-        field = sc.nextLine().toCharArray();
-        System.out.println("---------");
-        for (int i = 0; i < field.length; i += 3) {
-            System.out.println("| " + field[i] + " " + field[i + 1] + " " + field[i + 2] + " |");
+        String str = sc.nextLine();
+        int k = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = str.charAt(k++);
+            }
         }
         System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for ( int j = 0; j < 3; j++) {
+                System.out.print(field[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+        if (checkLine(field, 'X') && checkLine(field, 'O')) {
+            System.out.println("Impossible");
+        } else if (checkLine(field, 'X')) {
+            System.out.println("X wins");
+        } else if (checkLine(field, 'O')) {
+            System.out.println("O wins");
+        } else if (Math.abs(countChar(field, 'X') - countChar(field, 'O')) > 1) {
+            System.out.println("Impossible");
+        } else if (isFull(field)) {
+            System.out.println("Draw");
+        } else {
+            System.out.println("Game not finished");
+        }
+
     }
+
 }
